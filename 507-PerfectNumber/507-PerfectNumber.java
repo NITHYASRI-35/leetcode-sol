d5e001eaec96ecf460268ff0d7f3e10f69775a7b
NITHYASRI-35 <24149035@drngpit.ac.in>
2026-02-04 21:06:41 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,12 @@
+// Last updated: 2/4/2026, 9:06:41 PM
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i =1;i<=num/2;i++){
+        if(num%i==0)
+           sum+=i;
+        }
+        if(sum==num) return true;
+        else return false;   
+    }
+}
