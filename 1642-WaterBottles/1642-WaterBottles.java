91401b32ff1e0e3396a8de5f7909e77056f91837
NITHYASRI-35 <24149035@drngpit.ac.in>
2026-02-04 21:06:23 +0530

    Create 1642-WaterBottles

---
+++ 1642-WaterBottles/1642-WaterBottles.java
@@ -0,0 +1,14 @@
+// Last updated: 2/4/2026, 9:06:22 PM
+class Solution {
+    public int numWaterBottles(int NB ,int NE) {
+        int drink=NB;
+        int empty=NB;
+        while(empty>=NE){
+            int newD= empty/NE;
+            empty=(empty%NE) +newD;
+            drink+=newD;
+        }
+        return drink;
+
+    }
+}
